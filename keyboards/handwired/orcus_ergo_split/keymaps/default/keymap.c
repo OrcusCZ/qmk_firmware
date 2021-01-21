254e11686ed3be86d1dc82c21566340c437846a0
Josef Michálek <0rcus.cz@gmail.com>
2021-01-21 01:00:03 +0100

    Updated keymap

--- keyboards/handwired/orcus_ergo_split/keymaps/default/keymap.c
+++ keyboards/handwired/orcus_ergo_split/keymaps/default/keymap.c
@@ -30,14 +30,14 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, BL_TOGG, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-        _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, BL_STEP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
-        _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______, BL_BRTG, KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, _______,
-        _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ENT,  KC_MUTE, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
-        _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______,
-                          _______, _______,                                                       _______, _______,
-                          _______, _______,                                                       _______, _______,
-        _______, KC_SPC,  _______, _______,                                                       _______, _______, KC_SPC, _______
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_SCLN, BL_TOGG, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_KP_SLASH,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_QUOT, BL_STEP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_KP_ASTERISK,
+        _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_GRV,  BL_BRTG, KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_KP_MINUS,
+        _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ENT,  KC_MUTE, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_KP_PLUS,
+        KC_LCTL, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,                                     KC_LALT, KC_SLSH, KC_MINS, KC_LGUI, KC_KP_ENTER,
+                          KC_HOME, KC_PGUP,                                                       KC_BSPC, KC_UP,
+                          KC_END,  KC_PGDN,                                                       KC_DEL,  KC_DOWN,
+        KC_LSFT, KC_SPC,  _______, KC_INS,                                                        KC_LEFT, KC_RGHT, KC_SPC,  KC_ENTER
     ),
     // [_FN] = LAYOUT(
     //     QMKBEST, QMKURL,  _______,
